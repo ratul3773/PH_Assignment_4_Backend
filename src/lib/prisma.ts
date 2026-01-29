fd66f89219869ab292ab9e23f5e0e8dc76d0bbbf
Robiul Alam <ratul1036c@gmail.com>
2026-01-29 23:57:00 +0600

    creating database models

---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
+import { env } from "prisma/config";
+
+const connectionString = env("DATABASE_URL")
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+export { prisma }
